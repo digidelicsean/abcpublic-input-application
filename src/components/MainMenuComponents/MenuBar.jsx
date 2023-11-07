88fcd3ee8878cece03345e325da5e05bbde846ab
Sean Keiffer Lee <sean@digidelic.jp>
2023-11-07 16:31:17 +0800

    + Added a MenuBar component + Added a CardButton component

---
+++ src/components/MainMenuComponents/MenuBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function MenuBar() {
+  return (
+    <div style={style.menuBar}>
+        
+    </div>
+  )
+}
+
+
+const style = {
+    menuBar: {
+        
+    }
+}
+export default MenuBar
