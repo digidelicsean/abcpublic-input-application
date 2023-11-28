48e359807be137168c4e91890e03f11dc6a39c80
Sean Keiffer Lee <sean@digidelic.jp>
2023-11-28 17:02:29 +0800

    + Added simple implementation of TeamInfoTabPanel + Added module css for TeamInfoTabPanel + Added TeamInfoTabPanel to TeamPlayerSelectionPage

---
+++ src/components/TeamPlayerSelect/TeamInfoTabPanel.jsx
@@ -0,0 +1,13 @@
+import style from "./TeamInfoTabPanel"
+
+import { Tabs } from 'antd'
+
+const TeamInfoTabPanel = () => {
+    return (
+        <Tabs className={style.container}>
+
+        </Tabs>
+    )
+}
+
+export default TeamInfoTabPanel
--- src/pages/TeamPlayerSelect/TeamPlayerSelectionPage.jsx
+++ src/pages/TeamPlayerSelect/TeamPlayerSelectionPage.jsx
@@ -1,9 +1,10 @@
 import style from "TeamPlayerSelectionPage.module.css"
+import TeamInfoTabPanel from "../../components/TeamPlayerSelect/TeamInfoTabPanel"
 
 const TeamPlayerSelectionPage = () => {
   return (
     <div className={style.container}>
-        
+        <TeamInfoTabPanel/>
     </div>
   )
 }
