d25758b2695f3eb9c41f67192e041e538aeaf830
Sean Keiffer Lee <sean@digidelic.jp>
2023-12-11 10:43:27 +0800

    same as previous

--- src/components/TeamPlayerSelect/(tabs)/(player-info-tabs)/BattingResultTab.jsx
+++ src/components/TeamPlayerSelect/(tabs)/(player-info-tabs)/BattingResultTab.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import "../InfoTab.css"
+import style from "./BattingResultTab.module.css"
+import { Button } from 'antd'
+
+import LastUpdated from "../../../GeneralComponent/LastUpdated"
+
 function BattingResultTab() {
     return (
-        <div className='tab'>
-            BattingResultTab
+        <div className={`tab ${style.container}`}>
+
+            <div className={style.row} style={{
+                justifyContent: "space-between",
+            }}>
+                <div className={style['season-button-panel']}>
+                    <Button className={`${style['season-button-active']} ${style['season-button']}`}>今シーズン</Button>
+                    <Button className={style['season-button']}>作シーズン</Button>
+                    <Button className={style['season-button']}>生涯通算</Button>
+                </div>
+
+                <LastUpdated
+                    style={{ margin: "0px 5px", alignSelf: "flex-end" }}
+                    labelStyle={{ fontSize: "1em" }}
+                    inputStyle={{ fontSize: "1.4em", height: "32px", width: "120px" }}
+                />
+            </div>
         </div>
     )
-} 
- 
+}
+
+
 export default BattingResultTab
--- src/components/TeamPlayerSelect/PlayerInfoTabPanel.jsx
+++ src/components/TeamPlayerSelect/PlayerInfoTabPanel.jsx
@@ -3,6 +3,8 @@ import style from "./InfoTabPanel.module.css"
 import { ConfigProvider, Tabs } from 'antd'
 import { createTab } from "../../utils/tabUtils"
 
+import ButtonPanel from "./ButtonPanel";
+
 import {
     BattingResultTab,
     PitcherBallTab,
@@ -23,20 +25,23 @@ const tabProperties = {
 
 const PlayerInfoTabPanel = () => {
     const tabs = Object.entries(tabProperties).map(([label, children], index) => createTab(label, index, children));
-    const onTabChange = (key) => {};
-    
+    const onTabChange = (key) => { };
+
     return (
-        <ConfigProvider theme={{ components: { Tabs: { cardBg: "#d5d5d5", colorBgContainer: "#f4f4f4" } } }}>
-            <div className={style.container}>
-                <div className={style.backdrop} />
-                <Tabs
-                    onChange={onTabChange}
-                    type="card"
-                    items={tabs}
-                    tabBarStyle={{ margin: "0px" }}
-                />
-            </div>
-        </ConfigProvider>
+        <>
+            <ConfigProvider theme={{ components: { Tabs: { cardBg: "#d5d5d5", colorBgContainer: "#f4f4f4" } } }}>
+                <div className={style.container}>
+                    <div className={style.backdrop} />
+                    <Tabs
+                        onChange={onTabChange}
+                        type="card"
+                        items={tabs}
+                        tabBarStyle={{ margin: "0px" }}
+                    />
+                </div>
+            </ConfigProvider>
+            <ButtonPanel hasDelete/>
+        </>
     );
 }
 
