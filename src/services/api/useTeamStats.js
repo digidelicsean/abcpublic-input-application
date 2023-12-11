104b8117f71c8b5514fd6a47f25887f6c66db402
Sean Keiffer Lee <sean@digidelic.jp>
2023-12-11 21:32:56 +0800

    Added new api call to TeamStats

---
+++ src/services/api/useTeamStats.js
@@ -0,0 +1,42 @@
+import { useFetch, usePost } from "../../hooks/useFetch";
+
+export const useTeamStats = (teamCD) => {
+  const teamInfo = useFetch(`abc-public/total?Type=team`);
+
+  if (teamInfo.isLoading || teamInfo.error) {
+    return {
+      data: null,
+      reload: teamInfo.reload,
+    };
+  }
+
+  const parsedData = parseTeamStats(teamInfo.data, teamCD);
+
+  return {
+    data: parsedData,
+    reload: teamInfo.reload,
+    getByID: (playerCD) => {
+      return getPlayerByID(parsedData, playerCD);
+    },
+  };
+};
+
+const parseTeamStats = (data, teamCD) => {
+  if (data.length == 0) return [];
+
+  let gameClass = teamCD < 6 ? 1 : 2;
+  let teamStats = data[0][`TeamStats_${gameClass}`] ?? null;
+  console.log(data);
+  if (teamStats == null) return [];
+
+  return Object.values(teamStats).find((x) => x.TeamCD == teamCD);
+};
+
+const getPlayerByID = (directoryInfo, playerCD) => {
+  if (directoryInfo == null) {
+    return null;
+  }
+
+  const playerInfo = directoryInfo.find((x) => x.PlayerCD == playerCD);
+  return playerInfo;
+};
