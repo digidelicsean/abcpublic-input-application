b8656ab0d42612c147ff1b60e2825eab59e81fce
Francesca <francesca@digidelic.jp>
2023-12-12 17:23:28 +0800

    011 + Retrieve the data needed to display in the Match Card Component + Update the button labels and colors based on the "Situation" + Remove the button labels from AllMatchCard Component

--- src/components/AllMatchesComponents/MatchCard.jsx
+++ src/components/AllMatchesComponents/MatchCard.jsx
@@ -3,24 +3,57 @@ import { Button, Input, Card } from "antd";
 import "./MatchCard.css";
 import { fetchOtherGameInfoCollection } from './Data/otherGameInfoData';
 
-const MatchCard = ({ index, label, key, clicked, selected }) => {
-    const [stadiumName, setStadiumName] = useState();
-    const [teamNameV, setTeamNameV] = useState();
-    const [teamNameH, setTeamNameH] = useState();
+const MatchCard = ({ index, key, clicked, selected }) => {
+    const [stadiumName, setStadiumName] = useState("");
+    const [teamNameV, setTeamNameV] = useState("");
+    const [teamNameH, setTeamNameH] = useState("");
+    const [totalScoreV, setTotalScoreV] = useState("");
+    const [totalScoreH, setTotalScoreH] = useState("");
+    const [tb, setTB] = useState(0);
+    const [inning, setInning] = useState(0);
+    const [situation, setSituation] = useState();
     var otherGameInfoNum = parseInt(index) + 1;
 
     useEffect(() => {
-        fetchOtherGameInfoCollection().then(data => {
-            const otherGameInfo = data[0].OtherGameInfo[`OtherGameInfo_${otherGameInfoNum}`];
-            const stadium = otherGameInfo.Stadium;
-            const teamV = otherGameInfo.TeamName_V;
-            const teamH = otherGameInfo.TeamName_H;
+        const getOtherGameInfo = async () => {
+            await fetchOtherGameInfoCollection().then(data => {
+                const otherGameInfo = data[0].OtherGameInfo[`OtherGameInfo_${otherGameInfoNum}`];
+                const stadium = otherGameInfo.Stadium;
+                const teamV = otherGameInfo.TeamName_V;
+                const teamH = otherGameInfo.TeamName_H;
+                const totalV = otherGameInfo.Score_V.TotalSCore;
+                const totalH = otherGameInfo.Score_H.TotalSCore;
+                const inning = otherGameInfo.Inning;
+                const tb = otherGameInfo.TB;
+                const situation = otherGameInfo.Situation;
 
-            setStadiumName(stadium);
-            setTeamNameV(teamV);
-            setTeamNameH(teamH);
-        })
+                setStadiumName(stadium);
+                setTeamNameV(teamV);
+                setTeamNameH(teamH);
+                setTotalScoreV(totalV);
+                setTotalScoreH(totalH);
+                setTB(tb);
+                setInning(inning);
+                setSituation(situation);
+            });
+        };
+        getOtherGameInfo();
     }, [])
+
+    const tbValue = (tb) => {
+        if (tb === 1) return "表";
+        else if (tb === 2) return "裏";
+        else return "";
+    }
+
+    const btnLabel = (situation) => {
+        switch (situation) {
+            case 0: return "試合前";
+            case 1: return "試合中";
+            case 2: return "中止";
+            case 3: return "終了";
+        }
+    }
 
     return (
         <>
@@ -32,10 +65,10 @@ const MatchCard = ({ index, label, key, clicked, selected }) => {
                     <div className="other-game-top-body">
                         <div className="row1">
                             <Input value={stadiumName} />
-                            <Button className={`btn-${index}`}>{label}</Button>
+                            <Button className={`situation-${situation}`}>{btnLabel(situation)}</Button>
                         </div>
                         <div className="row2">
-                            <Input />
+                            <Input value={`${inning > 0 ? inning + "回" : ""}${tbValue(tb)}`} />
                             <Button className="sub-btn">-</Button>
                             <Button className="add-btn">+</Button>
                         </div>
@@ -44,7 +77,7 @@ const MatchCard = ({ index, label, key, clicked, selected }) => {
 
                         </div>
                         <div className="row4">
-                            <Input />
+                            <Input value={parseInt(totalScoreV) >= 0 ? totalScoreV : ""} />
                             <Button className="sub-btn">-</Button>
                             <Button className="add-btn">+</Button>
                         </div>
@@ -53,7 +86,7 @@ const MatchCard = ({ index, label, key, clicked, selected }) => {
 
                         </div>
                         <div className="row6">
-                            <Input />
+                            <Input value={parseInt(totalScoreH) >= 0 ? totalScoreH : ""} />
                             <Button className="sub-btn">-</Button>
                             <Button className="add-btn">+</Button>
                         </div>
