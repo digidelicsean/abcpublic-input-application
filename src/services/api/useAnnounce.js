9ec0d739d52f030659befafd9721c5cad27eaa09
Sean Keiffer Lee <sean@digidelic.jp>
2023-12-18 14:16:03 +0800

    Added new api call to announce data. + Added new script that has an api call to the announce data.

---
+++ src/services/api/useAnnounce.js
@@ -0,0 +1,44 @@
+import { useFetch } from "../../hooks/useFetch";
+
+export const useAnnounce = () => {
+  const announceData = useFetch("abc-public/announce");
+
+  if (announceData.isLoading || announceData.error) {
+    return {
+      data: null,
+      reload: announceData.reload,
+      getTradeInfo: (date) => [],
+      getEntryInfo: (date) => [],
+    };
+  }
+
+  const parsedData = parseAnnounceData(announceData.data);
+  return {
+    data: announceData.data,
+    reload: announceData.reload,
+    getTradeInfo: (date) => {
+      return getPlayerTradeInfo(parsedData, date);
+    },
+    getEntryInfo: (date) => {
+      return getPlayerEntryInfo(parsedData, date);
+    },
+  };
+};
+
+const parseAnnounceData = (data) => {
+  if (!data) return [];
+  return Object.values(data);
+};
+
+const getPlayerTradeInfo = (data, date) => {
+    if(!data) return []
+    console.log(data)
+    return data.find((tradeInfo) => tradeInfo.Type == `PlayerTrade_${date}`)
+};
+
+const getPlayerEntryInfo = (data, date) => {
+    if(!data) return []
+    const playerEntryInfo = data.find((tradeInfo) => tradeInfo.Type == `PlayerEntry_${date}`)
+
+    return playerEntryInfo ? playerEntryInfo[`PlayerEntry_${date}`] : []
+};
