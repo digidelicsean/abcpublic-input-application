e27b8a0f312c8715da48ee2de2b08fe17f0caac0
Sean Keiffer Lee <seankeiffer@gmail.com>
2023-12-27 10:47:24 +0800

    Added comments to data fetch scripts

--- src/services/api/usePlayerTradeInfo.js
+++ src/services/api/usePlayerTradeInfo.js
@@ -1,8 +1,9 @@
 import { useFetch } from "../../hooks/useFetch";
-
 export const usePlayerTradeInfo = () => {
+  // Fetch data from the "abc-public/announce" endpoint
   const announceData = useFetch("abc-public/announce");
 
+  // If the data is still loading or there's an error, return null values
   if (announceData.isLoading || announceData.error) {
     return {
       data: null,
@@ -11,25 +12,32 @@ export const usePlayerTradeInfo = () => {
     };
   }
 
+  // Parse the announce data into a usable format
   const parsedTradeInfoData = parseAnnounceData(announceData.data);
 
+  // Return the parsed data along with other functions and values
   return {
     data: parsedTradeInfoData,
     reload: announceData.reload,
     getPlayerTradeInfo: (playerCD) => {
+      // Get trade info for a specific player
       return getPlayerTradeInfo(parsedTradeInfoData, playerCD);
     },
   };
 };
 
+// Function to parse the announce data
 const parseAnnounceData = (data) => {
+  // If the data is empty, return an empty array
   if (!data) return [];
 
+  // Filter and sort the data to get player trade information
   let playerTradeInfos = Object.values(data).filter((x) =>
     x.Type.includes("PlayerTrade")
   );
   playerTradeInfos = playerTradeInfos
     .sort((a, b) => {
+      // Helper function to get the announce date from a data object
       const getAnnounceDate = (data) => {
         const dataType = data?.Type;
         const dataValue = data?.[dataType];
@@ -45,24 +53,31 @@ const parseAnnounceData = (data) => {
         }
         return null;
       };
+
+      // Get the announce dates for the trade info objects
       const aDate = getAnnounceDate(a);
       const bDate = getAnnounceDate(b);
 
+      // Sort the objects based on the announce dates in descending order
       return bDate - aDate;
     })
     .slice(0, 3);
 
+  // Return the sorted player trade info objects
   return playerTradeInfos;
 };
 
+// Function to get trade info for a specific player
 const getPlayerTradeInfo = (data, playerCD) => {
+  // If the data is empty, return an empty array
   if (!data) return [];
+
   let playerTradeInfoData = [];
   for (let i = 0; i < data.length; i++) {
     const playerTradeInfo = data[i];
     const tradeInfoData = playerTradeInfo[playerTradeInfo.Type];
-    if (tradeInfoData.TradeInfo[`PlayerTrade_${playerCD}`]) 
-    {
+    if (tradeInfoData.TradeInfo[`PlayerTrade_${playerCD}`]) {
+      // Get the trade info for the specified player
       playerTradeInfoData = tradeInfoData.TradeInfo[`PlayerTrade_${playerCD}`];
       playerTradeInfoData.AnnounceDate = tradeInfoData.AnnounceDate;
       playerTradeInfoData.SendDate = tradeInfoData.SendDate;
@@ -71,5 +86,6 @@ const getPlayerTradeInfo = (data, playerCD) => {
     }
   }
 
+  // Return the trade info for the specified player
   return playerTradeInfoData;
 };
